4c4a8d410fbed63ed9c0922e8a5bcf7640376a71
sandazzz <rsandarisoa@gmail.com>
2024-06-26 00:15:38 +0200

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import TheHeader from './components/AppHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <RouterView />
+</template>
+
+<style scoped></style>
