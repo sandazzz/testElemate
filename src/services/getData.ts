80852113f2cdad1d1c2396a897bb6ea64926c7c5
sandazzz <rsandarisoa@gmail.com>
2024-07-01 05:59:43 +0200

    Update getData.ts
    adding a comment about the https requests

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -6,6 +6,7 @@ export function getCurrentWeather(name: string): Promise<CurrentWeather | null> 
   const apiKey = import.meta.env.VITE_API_WEATHER_KEY
 
   // Effectue une requête fetch vers l'API weatherstack avec le nom de la ville et la clé API
+  //!!! Le plan gratuit permet que des requettes http, les requettes https sont payantes
   const data = fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${name}`)
     .then((response) => response.json())
     .then((json) => {
